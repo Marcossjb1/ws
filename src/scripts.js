d1e5c1465a8a22884d530378c6c41fb7513bf105
Marco Antônio Mafessolli <marcomafessolli@gmail.com>
2022-01-22 14:28:01 -0300

    style: 💄 padroniza identação dos arquivos

--- src/scripts.js
+++ src/scripts.js
@@ -1,8 +1,3 @@
-document
-.querySelector("button.fat")
-.addEventListener("click", function(){
-    document
-        .querySelector("#footer")
-        .classList
-        .toggle("hide")
-})
+document.querySelector("button.fat").addEventListener("click", function () {
+  document.querySelector("#footer").classList.toggle("hide");
+});
